bbcd208ece88de21a9aca2e1aecf1b28615ea0ed
Abdul Rehman <a_rehmanhameed@hotmail.com>
2018-06-08 13:02:38 +0300

    Added Search Fragment
    Here user can search any content he want to see

---
+++ UTube/app/src/main/java/io/boringideas/utube/SearchFragment.java
@@ -0,0 +1,24 @@
+package io.boringideas.utube;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SearchFragment extends Fragment {
+
+    public SearchFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+}
