e952baa968d281999434861652e814a32b44d30a
Abdul Rehman <a_rehmanhameed@hotmail.com>
2018-06-08 13:01:19 +0300

    Added Favorite Fragment
    Responsible to show favorite items

---
+++ UTube/app/src/main/java/io/boringideas/utube/FavoriteFragment.java
@@ -0,0 +1,27 @@
+package io.boringideas.utube;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class FavoriteFragment extends Fragment {
+
+    public FavoriteFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_favorite, container, false);
+    }
+  }
