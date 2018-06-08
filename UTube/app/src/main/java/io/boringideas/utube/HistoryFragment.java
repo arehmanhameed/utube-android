b9c83c8bc7c2eb6e3e0f4eef3ba7a5f1505b3e71
Abdul Rehman <a_rehmanhameed@hotmail.com>
2018-06-08 13:01:48 +0300

    Added History Fragment
    Here user can see what items he have seen

---
+++ UTube/app/src/main/java/io/boringideas/utube/HistoryFragment.java
@@ -0,0 +1,27 @@
+package io.boringideas.utube;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class HistoryFragment extends Fragment {
+
+    public HistoryFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+}
